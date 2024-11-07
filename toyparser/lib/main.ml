05b8dff12c05b19dd4005f573539e49ebbad5b41
Docker <docker@example.com>
2024-11-07 08:37:58 +0000

    Toyparser finito

--- toyparser/lib/main.ml
+++ toyparser/lib/main.ml
@@ -22,37 +22,22 @@ let string_of_intorerr : int_or_err -> string = function
 let rec eval : ast -> int_or_err = function
   | Const n -> Ok n
   | Add (e1, e2) ->
-    let res1 = eval e1 in
-    let res2 = eval e2 in
-    (match res1, res2 with
-     | Error err1, _ -> Error err1
-     | _, Error err2 -> Error err2
-     | Ok v1, Ok v2 -> Ok (v1 + v2))
+    eval e1 ==> fun v1 ->
+    eval e2 ==> fun v2 ->
+    Ok (v1 + v2)
   | Diff (e1, e2) ->
-    let res1 = eval e1 in
-    let res2 = eval e2 in
-    (match res1, res2 with
-     | Error err1, _ -> Error err1
-     | _, Error err2 -> Error err2
-     | Ok v1, Ok v2 ->  Ok (v1 - v2))
-  | Negs (e1) ->
-    let res1 = eval e1 in
-    (match res1 with
-      | Error err1 -> Error err1
-      | Ok v1 ->  Ok (-v1))   
+    eval e1 ==> fun v1 ->
+    eval e2 ==> fun v2 ->
+    Ok (v1 - v2)
+  | Negs e1 ->
+    eval e1 ==> fun v1 ->
+    Ok (-v1)
   | Molt (e1, e2) ->
-    let res1 = eval e1 in
-    let res2 = eval e2 in
-    (match res1, res2 with
-      | Error err1, _ -> Error err1
-      | _, Error err2 -> Error err2
-      | Ok v1, Ok v2 -> Ok (v1 * v2))
+    eval e1 ==> fun v1 ->
+    eval e2 ==> fun v2 ->
+    Ok (v1 * v2)
   | Divs (e1, e2) ->
-    let res1 = eval e1 in
-    let res2 = eval e2 in
-    (match res1, res2 with
-      | Error err1, _ -> Error err1
-      | _, Error err2 -> Error err2
-      | Ok v1, Ok v2 ->
-        if v2 = 0 then Error "Division by zero"
-        else Ok (v1 / v2))      
+    eval e1 ==> fun v1 ->
+    eval e2 ==> fun v2 ->
+    if v2 = 0 then Error "Division by zero"
+    else Ok (v1 / v2)
