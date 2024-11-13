e6b9f1cb3f950168e83c4c2a5886448bc122b3c9
Docker <docker@example.com>
2024-11-13 15:56:48 +0000

    Boolexpr (manca il task 6)

--- expr/boolexpr/lib/main.ml
+++ expr/boolexpr/lib/main.ml
@@ -5,19 +5,16 @@ let rec string_of_boolexpr = function
   | False -> "False"
   | If(e0,e1,e2) -> "If(" ^ (string_of_boolexpr e0) ^ "," ^ (string_of_boolexpr e1) ^ "," ^ (string_of_boolexpr e2) ^ ")"
 
-
 let parse (s : string) : boolExpr =
   let lexbuf = Lexing.from_string s in
   let ast = Parser.prog Lexer.read lexbuf in
   ast
-
-
 exception NoRuleApplies
 
 let rec trace1 = function
     If(True,e1,_) -> e1
   | If(False,_,e2) -> e2
-  | If(_,_,_) -> failwith "TODO"
+  | If(e1, e2, e3) -> let e1' = trace1 e1 in If(e1', e2, e3)
   | _ -> raise NoRuleApplies
 
 let rec trace e = try
@@ -25,8 +22,9 @@ let rec trace e = try
     in e::(trace e')
   with NoRuleApplies -> [e]
 
-
-let rec eval = function
-    True -> true
+  let rec eval = function
+  | True -> true
   | False -> false
-  | If(_,_,_) -> failwith "TODO"
+  | If(e1, e2, e3) -> match eval e1 with
+    | true -> eval e2
+    | false -> eval e3
--- expr/boolexpr/test/boolexpr.ml
+++ expr/boolexpr/test/boolexpr.ml
@@ -1,12 +1,46 @@
 open BoolexprLib.Main
+open BoolexprLib.Ast
 
 let test_eval expr exp_result =
   (expr |> parse |> eval) = exp_result
 
 (* ### Unit tests for task 4 *)
-
-let%test "test_eval_1" = failwith "TODO"
+let%test "test_eval_false" = test_eval "false" false
+let%test "test_eval_true" = test_eval "true" true
+let%test "test_eval_if_true_then_false_else_true" = test_eval "if true then false else true" false
+let%test "test_eval_if_false_then_false_else_true" = test_eval "if false then false else true" true
+let%test "test_eval_nested_if_1" = test_eval "if true then (if true then false else true) else (if true then true else false)" false
+let%test "test_eval_nested_if_2" = test_eval "if (if false then false else false) then (if false then true else false) else (if true then false else true)" false
+let%test "test_eval_nested_if_3" = test_eval "if (if (if false then false else false) then (if false then true else false) else (if true then false else true)) then (if false then true else false) else (if true then false else true)" false
 
 (* ### Unit tests for task 5 *)
+let%test "trace1_if_true_then_false" = 
+  let expr = parse "if true then false else true" in
+  trace1 expr = parse "false"
 
-let%test "test_trace1_1" = failwith "TODO"
+let%test "trace1_if_false_then_true" = 
+  let expr = parse "if false then false else true" in
+  trace1 expr = parse "true"
+
+let%test "trace1_nested_if" = 
+  let expr = parse "if true then (if true then false else true) else (if true then true else false)" in
+  trace1 expr = parse "if true then false else true"
+
+let%test "trace1_no_progress_is_value_true" = 
+  let expr = parse "true" in
+  try 
+    let _ = trace1 expr in 
+    false 
+  with NoRuleApplies -> is_value expr
+
+let%test "trace1_no_progress_is_value_false" = 
+  let expr = parse "false" in
+  try 
+    let _ = trace1 expr in 
+    false 
+  with NoRuleApplies -> is_value expr
+
+let%test "trace_fully_reduced_in_10_steps" = 
+  let expr = parse "if (if false then false else false) then (if false then true else false) else (if true then false else true)" in
+  let steps = trace expr in
+  List.length steps <= 10
